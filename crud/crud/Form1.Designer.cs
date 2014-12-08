3d95be757728e5a3389348bc12d101cd766012db
diegoaraujoresende <araujo.resende@gmail.com>
2014-12-08 16:20:20 -0300

    Modify Teste
    Teste

--- crud/crud/Form1.Designer.cs
+++ crud/crud/Form1.Designer.cs
@@ -99,7 +99,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(76, 13);
+            this.label3.Location = new System.Drawing.Point(85, 17);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(363, 31);
             this.label3.TabIndex = 6;
