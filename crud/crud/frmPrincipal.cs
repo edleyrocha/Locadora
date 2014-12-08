64ac7003dacea0763f2a3d76dd028ff03c3fc119
Edley Allan Hoyler <edleyrocha@hotmail.com>
2014-12-08 16:44:01 -0300

    xxxx
    xxxx

--- crud/crud/frmPrincipal.cs
+++ crud/crud/frmPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace crud
 {
-    public partial class formprincipal : Form
+    public partial class frmPrincipal : Form
     {
-        public formprincipal()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
